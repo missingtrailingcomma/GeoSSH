4bde16e6770acf9276182d379c4d421aaff8ab65
Yizheng Shen <tinirdoc@gmail.com>
2015-08-10 23:36:34 -0500

    setup.sh functional now [._.]

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo -e "Enter File Name: \c"
+echo -e "Enter File Absolute Path: \c"
 read filename
+touch $filename
 echo "ip,lat,lon,city,time" >> $filename
-echo "ip=env | grep SSH_CLIENT | grep -o '\([[:digit:]]\+\.\)\{3\}[[:digit:]]\+'" >> ~/.bashrc
-echo 'echo -e `env | grep SSH_CLIENT | grep -o \'\\([[:digit:]]\\+\\.\\)\\{3\\}[[:digit:]]\\+\'`\c >>'" $filename" >> ~/.bashrc
-echo 'echo -e `curl "ipinfo.io/"$ip"/loc"`",\c" >>'" $filename" >> ~/.bashrc
-echo 'echo -e `curl "ipinfo.io/"$ip"/city"`",\c" >>'" $filename" >> ~/.bashrc
-echo "date +%Y/%m/%d\ %H:%M:%S >> $filename" >> ~/.bashrc
+echo "ip=env | grep SSH_CLIENT | grep -o '\([[:digit:]]\+\.\)\{3\}[[:digit:]]\+'" >> ~/Desktop/sample.txt
+echo -e 'echo -e `env | grep SSH_CLIENT \c' >> ~/Desktop/sample.txt
+echo -e "| grep -o '\([[:digit:]]\+\.\)\{3\}[[:digit:]]\+'\c" >> ~/Desktop/sample.txt
+echo '`",\c" >> '"$filename" >> ~/Desktop/sample.txt
+echo 'echo -e `curl "ipinfo.io/$ip/loc"`",\c" >>'" $filename" >> ~/Desktop/sample.txt
+echo 'echo -e `curl "ipinfo.io/$ip/city"`",\c" >>'" $filename" >> ~/Desktop/sample.txt
+echo "date +%Y/%m/%d\ %H:%M:%S >> $filename" >> ~/Desktop/sample.txt
